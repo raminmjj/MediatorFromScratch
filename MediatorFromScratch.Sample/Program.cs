6e442003d6c2adff6f7d44347aaf1fd390329032
Ramin Alirezaee <ramin.alirezaee@hotmail.com>
2021-08-31 18:32:06 +0430

    Add project files.

---
+++ MediatorFromScratch.Sample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MediatorFromScratch.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
